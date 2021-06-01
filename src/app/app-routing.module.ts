fce39599ed56d4d054656e7daba4b52d5a9ac17b
Appedro <appedro.dev@gmail.com>
2021-06-01 09:28:32 -0300

    define routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { GalleryComponent } from './gallery/gallery.component';
+import { ArticleComponent } from './article/article.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: ArticleComponent
+},
+{
+  path: 'gallery',
+  component: GalleryComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
